49d04dc0a9823cd99d0663cf6eef69c5ff050dc1
wbruxy <brunaa0407@gmail.com>
2023-09-19 19:39:13 -0300

    começo de routes e views

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+//Route::get('/', function () {
+//   return view('welcome');
+//});
+
+
+Route::get('/', [App\Http\Controllers\IndexController::class, 'index'])->name('index');
